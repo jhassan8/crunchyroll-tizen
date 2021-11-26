7d30a00c02488b55fa678dd0287158ffa372c334
Jocs <jhassan8@outlook.com>
2021-11-25 22:52:06 -0300

    added basic error screen

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
-var app = {};
+var app = {
+  state: false,
+};
 
-window.onload = function () {};
+window.onload = function () {
+  app.initError();
+};
+
+app.initError = function () {
+  app.state = false;
+  document.getElementById("error-screen").style.display = "flex";
+};
 
 window.onunload = function () {};
 
-app.keyDown = function (e) {};
+app.keyDown = function (e) {
+  switch (e.keyCode) {
+    case tvKey.KEY_RETURN:
+    case tvKey.KEY_EXIT:
+      tizen.application.getCurrentApplication().hide();
+      break;
+  }
+};
