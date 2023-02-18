a57bffc855caf3c42e50400406ebc93830a5e673
Jocs <jhassan8@outlook.com>
2023-02-17 22:50:03 -0300

    added home details
    - added UI with data details and small mode

--- server/js/screen/home.js
+++ server/js/screen/home.js
@@ -3,18 +3,32 @@ var home = {
   data: {
     main: NaN,
   },
+  position: 0,
 };
 
 home.init = function () {
-  menu.init();
   var home_element = document.createElement("div");
   home_element.id = home.id;
 
   home_element.innerHTML = `
   <div class="content">
+    <div class="details full">
+      <div class="background">
+        <img src="${home.data.main.banner.background}">
+      </div>
+      <div class="info">
+        <div class="title resize">${home.data.main.banner.title}</div>
+        <div class="description resize">${home.data.main.banner.description}</div>
+        <div class="buttons">
+          <a class="selected">Reproducir</a>
+          <a>Mas Informacion</a>
+        </div>
+      </div>
+    </div>
   </div>`;
 
   document.body.appendChild(home_element);
+  menu.init();
   main.state = home.id;
 };
 
@@ -35,8 +49,24 @@ home.keyDown = function (event) {
     case tvKey.KEY_DOWN:
       break;
     case tvKey.KEY_LEFT:
+      if (home.position > 0) {
+      } else {
+        var buttons = $(".details .buttons a");
+        var current = buttons.index($(`.details .buttons a.selected`));
+        buttons.removeClass("selected");
+        buttons.eq(current > 0 ? current - 1 : current).addClass("selected");
+      }
       break;
     case tvKey.KEY_RIGHT:
+      if (home.position > 0) {
+      } else {
+        var buttons = $(".details .buttons a");
+        var current = buttons.index($(`.details .buttons a.selected`));
+        buttons.removeClass("selected");
+        buttons
+          .eq(current < buttons.length - 1 ? current + 1 : current)
+          .addClass("selected");
+      }
       break;
     case tvKey.KEY_ENTER:
     case tvKey.KEY_PANEL_ENTER:
