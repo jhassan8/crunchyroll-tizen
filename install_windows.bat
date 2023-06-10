ea418bd1da676b20a70861c1137af55050b6c57f
Jocs <jhassan8@outlook.com>
2023-06-10 02:01:14 -0300

    chore: update window installer

--- install_windows.bat
+++ install_windows.bat
@@ -1,10 +1,15 @@
 @echo off
 
-set /p "tizen_directory=Enter Tizen path (default: \tizen-studio): "
+echo Welcome!
+echo Before proceeding with the installation of the Tizen application,
+echo make sure you have configured your device as a developer and have Tizen Studio installed.
+echo.
+
+set /p "tizen_directory=Enter Tizen full path (default: \tizen-studio): "
 if "%tizen_directory%"=="" set "tizen_directory=\tizen-studio"
 
-set /p "app_directory=Enter .wgt app path (default: Crunchyroll.wgt): "
-if "%app_directory%"=="" set "app_directory=Crunchyroll.wgt"
+set /p "app_directory=Enter .wgt app full path (default: crunchyroll_online.wgt): "
+if "%app_directory%"=="" set "app_directory=%~dp0crunchyroll_online.wgt"
 
 set /p "device_ip=Enter device IP (example: 192.168.0.37): "
 
@@ -16,7 +21,7 @@ if "%device_ip%"=="" (
 cd %tizen_directory%/tools
 sdb connect %device_ip%
 for /f "tokens=3" %%a in ('sdb devices ^| findstr /c:"device"') do set "device_name=%%a"
-cd %tizen_directory%/tools/ide/bin
+cd ide/bin
 tizen install -t %device_name% --name %app_directory%
 
 pause
