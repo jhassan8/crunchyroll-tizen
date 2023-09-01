8aa20a21990abe4fde88bf888104c4c83b9afc5d
Jocs <jhassan8@outlook.com>
2023-09-01 19:57:15 -0300

    browse update

--- server/js/screen/browse.js
+++ server/js/screen/browse.js
@@ -1,5 +1,8 @@
 window.browse = {
   id: "browse-screen",
+  data: {
+    categories: [],
+  },
 
   init: function () {
     var browse_element = document.createElement("div");
@@ -8,18 +11,20 @@ window.browse = {
     loading.start();
     service.categories({
       success: function (response) {
+        browse.data.categories = response.items;
         var elements = "";
-        response.items.forEach((element) => {
+        browse.data.categories.forEach((element, index) => {
           elements += `
-          <li class="item">
+          <li class="item${index === 0 ? " focus" : ""}">
+            <img src="${element.images.low[0].source}"/>
             ${element.localization.title}
           </li>`;
         });
 
         browse_element.innerHTML = `
         <div class="content">
+          <img id="browse-background"/>
           <div id="browse-menu">
-            <div class="title">Browse</div> 
             <div class="browse-content">
               <ul class="browse-content-wrapper">
                 ${elements}
@@ -31,6 +36,7 @@ window.browse = {
         document.body.appendChild(browse_element);
         menu.destroy();
         loading.end();
+        browse.move("up");
       },
       error: function (error) {
         loading.end();
@@ -47,7 +53,41 @@ window.browse = {
     switch (event.keyCode) {
       case tvKey.KEY_BACK:
       case 27:
+        // ------
+        break;
+      case tvKey.KEY_UP:
+        browse.move("up");
+        break;
+      case tvKey.KEY_DOWN:
+        browse.move("down");
         break;
     }
+  },
+
+  move: function (direction) {
+    var options = $(".browse-content .item");
+    var current = options.index($(".browse-content .item.focus"));
+
+    options.removeClass("focus");
+
+    if (direction === "up") {
+      var newCurrent = current > 0 ? current - 1 : current;
+    } else {
+      var newCurrent = current < options.length - 1 ? current + 1 : current;
+    }
+
+    options.eq(newCurrent).addClass("focus");
+    $("#browse-background").attr(
+      "src",
+      browse.data.categories[newCurrent].images.background[4].source
+    );
+    
+    var marginTop = 0;
+    var max = 9;
+    if (options.length > max && newCurrent > max - 1) {
+      marginTop = -((newCurrent - (max - 1)) * 110);
+    }
+
+    $(".browse-content-wrapper")[0].style.marginTop = `${marginTop}px`;
   },
 };
